038e15d8709e0d873e45233799fe039b10a4c4ef
Akhilesh <akhivvvv@gmail.com>
2021-05-31 13:16:17 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import "./App.css";
+import LoginScree from "./LoginScree";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+//import { useStateValue } from './StateProvider'
+import React, { useState } from "react";
 
-import './App.css';
-import LoginScree from './LoginScree';
+
 
 function App() {
+ 
+  const [userr, setUserr] = useState("");
+  console.log(userr);
   return (
-    <LoginScree/>
-    
+    <div>
+      <Router>
+        {!userr ? (
+          <LoginScree onClick={value => setUserr(value)}/>
+        ) : (
+          <>
+            <Switch>
+              <Route path="/" exact>
+                <Home onClick={value => setUserr(value)}/>
+              </Route>
+            </Switch>
+          </>
+        )}
+      </Router>
+    </div>
   );
 }
 
--- src/LoginScree.js
+++ src/LoginScree.js
@@ -1,64 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import qs from "qs";
 import axios from "axios";
 import "./LoginScree.css";
+import { useStateValue } from "./StateProvider";
 
-export default function LoginScree() {
+const LoginScree = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   let [responseData, setResponseData] = React.useState("");
+  let [responsetoken, setResponseToken] = React.useState("");
+  const [{ user }, dispatch] = useStateValue();
+  const [isLoading, setLoading] = useState(true);
 
-  const clicked = function (event) {
-    if (username !== "") {
-      axios({
-        method: "post",
-        url: "http://localhost:4000/auth/signup",
-        data: qs.stringify({
-          email: email,
-          password: password,
-          username: username,
-        }),
-        headers: {
-          "content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
+  const Sclicked = function (event) {
+    axios({
+      method: "post",
+      url: "http://medha.dachrs.com:8000/accounts/register/",
+      data: qs.stringify({
+        email: email,
+        password: password,
+        username: username,
+      }),
+      headers: {
+        "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    })
+      .then(function (response) {
+        setResponseData(response.data.message);
       })
-        .then(function (response) {
-          setResponseData(response.data.message);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    } else {
-      axios({
-        method: "post",
-        url: "http://localhost:4000/auth/signin",
-        data: qs.stringify({
-          email: email,
-          password: password,
-        }),
-        headers: {
-          "content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+  const Clicked = function (event) {
+    axios({
+      method: "post",
+      url: "http://medha.dachrs.com:8000/accounts/login/",
+      data: qs.stringify({
+        email: email,
+        password: password,
+      }),
+      headers: {
+        "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    })
+      .then(function (response) {
+        // console.log(response.data.token);
+        setResponseToken(response.data.token);
+        setResponseData(response.data.message);
+        setLoading(false);
+        props.onClick(response.data.token);
       })
-        .then(function (response) {
-          console.log(response.data.token);
-          setResponseData(response.data.message);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    if (responseData === "You are signed in successfully! Token Genrated") {
+      dispatch({
+        type: "SET_USER",
+        user: responsetoken,
+      });
+       console.log(responsetoken)
     }
   };
 
   return (
     <div className="Form">
       <h1>User Login Screen</h1>
+
       <input
         type="email"
         className="input"
         id="email"
-        placeholder="Enter email"
+        placeholder="Enter Email to Sign Up"
         required
         onChange={(e) => setEmail(e.target.value)}
       ></input>
@@ -66,7 +81,7 @@ export default function LoginScree() {
         className="input"
         type="name"
         id="email"
-        placeholder="Enter userName"
+        placeholder="username"
         required
         onChange={(e) => setUsername(e.target.value)}
       ></input>
@@ -74,14 +89,21 @@ export default function LoginScree() {
         type="password"
         className="input"
         id="password"
-        placeholder="Enter password"
+        placeholder="password"
         required
         onChange={(e) => setPassword(e.target.value)}
       ></input>
-      <button className="input" onClick={clicked}>
-        SignUp / SignIn
-      </button>
+      <div className="twobuttons">
+        <button className="input" onClick={Clicked}>
+          Sign In
+        </button>
+        <button className="input" onClick={Sclicked}>
+          Sign Up
+        </button>
+      </div>
+
       <p>Response from server: {responseData} </p>
     </div>
   );
-}
+};
+export default LoginScree;
